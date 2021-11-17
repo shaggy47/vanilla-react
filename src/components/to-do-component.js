1d4421faea1916773040ab89f278e1c1c6305344
vaibhavpfatkal@gmail.com <vaibhavpfatkal@gmail.com>
2021-11-17 12:39:25 +0530

    Dom manipulation

--- src/components/to-do-component.js
+++ src/components/to-do-component.js
@@ -1,26 +1,38 @@
 import React from 'react';
 
 class ToDoComponent extends React.Component {
+    constructor() {
+        super();
+        this.addToList = this.addToList.bind(this);
+        this.state = { listItems: ['Abc', 'DEF'] };
+    }
+
     render() {
+        const list = []; 
+        this.state.listItems.map((item,index)=>{
+            list.push(<li key={index}>{item}</li>);
+        });
+        
+        console.log(list);
         return (
             <div>
-            <span>{this.props.name} this is your to do list</span>
-            <ul id="list">
-                <li>Groceries</li>
-                <li>Bills</li>
-                <li>childcare</li>
-                <li>Doctor visit</li>
-            </ul>
-            <br/>
-            <p>Click here to add another task</p>
-            <input type="text"></input>               
-            <button id="btn" onClick={this.addToList()}>Add</button>
-        </div>
+                <span>{this.props.name} this is your to do list</span>
+                <ul>
+                    {list}
+                </ul>
+                <br />
+                <p>Click here to add another task</p>
+                <input type="text" id="todoItem"></input>
+                <button id="btn" onClick={this.addToList}>Add</button>
+            </div>
         )
     }
 
-    addToList(){
-        console.log('Added to list');        
+    addToList(e) {
+        const list = this.state.listItems;
+        const todoItem = document.getElementById('todoItem');
+        list.push(todoItem.value);
+        this.setState({ listItems: list });
     }
 
 }
