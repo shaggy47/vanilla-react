b17b2314d18d09f5e3180b2751ecb5dd3a31c70f
vaibhavpfatkal@gmail.com <vaibhavpfatkal@gmail.com>
2021-11-22 18:47:32 +0530

    Editable table

--- src/components/table-component.js
+++ src/components/table-component.js
@@ -8,6 +8,7 @@ class TableComponent extends React.Component {
         this.sort = this.sort.bind(this);
         this.clone = this.clone.bind(this);
         this.showEditor = this.showEditor.bind(this);
+        this.presSearchData = null;
     }
 
     sort(e) {
@@ -32,7 +33,7 @@ class TableComponent extends React.Component {
 
     showEditor(e) {
         let columnName = String(this.props.headers[e.target.cellIndex]).toLowerCase();
-        console.log(columnName);
+
         this.setState({
             edit: {
                 row: parseInt(e.target.parentNode.dataset.row, 10),
@@ -41,6 +42,38 @@ class TableComponent extends React.Component {
         });
 
         console.log(this.state);
+    }
+
+    save(e, edit) {
+        e.preventDefault();
+        const input = e.target.firstChild;
+        const data = this.clone(this.state.data);
+        data[edit.row][edit.column] = input.value;
+        this.setState({
+            data: data,
+            edit: null
+        });
+    }
+
+    searchBy(e, columnIndex) {
+        console.log(this.presSearchData);
+        const term = e.target.value.toLowerCase();
+        this.presSearchData = this.state.data;
+        if (term.length==0) {
+            this.setState({
+                data: this.presSearchData,
+                edit: null
+            });
+            return;
+        }
+
+        const property = String(this.props.headers[columnIndex]).toLowerCase();
+        const data = this.clone(this.state.data.filter(d => String(d[property]).toLowerCase().indexOf(term) > -1));
+        this.presSearchData = this.state.data;
+        this.setState({
+            data: data,
+            edit: null
+        })
     }
 
     render() {
@@ -58,16 +91,32 @@ class TableComponent extends React.Component {
                 <table>
                     <thead onClick={this.sort}>
                         <tr>{headers}</tr>
+                        <tr>{
+                            headers.map((_, index) => (
+                                <td key={'search_' + index}>
+                                    <input type="text" onChange={(e) => { this.searchBy(e, index) }} key={index}></input>
+                                </td>
+                            ))
+                        }</tr>
                     </thead>
                     <tbody onDoubleClick={this.showEditor}>
                         {
-                            this.state.data.map((row, index) => (
-                                <tr data-row={index} key={index}>
-                                    <td>{row.title}</td>
-                                    <td>{row.author}</td>
-                                    <td>{row.language}</td>
-                                    <td>{row.published}</td>
-                                    <td>{row.sales}</td>
+                            this.state.data.map((row, rowIndex) => (
+                                <tr key={rowIndex} data-row={rowIndex}>
+                                    {
+                                        Object.keys(row).map((key, index) => {
+                                            const edit = this.state.edit;
+                                            if (edit && edit.row === rowIndex && key === edit.column) {
+                                                return <td key={index + edit.column}>
+                                                    <form onSubmit={(e) => { this.save(e, this.state.edit) }}>
+                                                        <input type="text" defaultValue={row[key]} />
+                                                    </form>
+                                                </td>
+                                            }
+
+                                            return <td key={index}>{row[key]}</td>
+                                        })
+                                    }
                                 </tr>
                             ))
                         }
